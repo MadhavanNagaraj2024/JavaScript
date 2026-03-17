23180f7f7352ad02eb5f11f2200c37292ed8ea27
MadhavanNagaraj2024 <madhavannagaraj.dev@gmail.com>
2026-03-17 17:50:17 +0530

    refactor: enhance WeakMap.js with detailed comments and structured logging

--- Js/WeakMap/WeakMap.js
+++ Js/WeakMap/WeakMap.js
@@ -1,7 +1,35 @@
-const wm = new WeakMap();
+let wm = new WeakMap();
 
-const user = { name: "Tom" };
+let user = { name: "Tom" };
 
-wm.set(user, "User Data");
+// set
+wm.set(user, "data");
 
-console.log(wm.get(user));
+// get
+console.log("Get", wm.get(user));
+
+// has
+console.log("Has", wm.has(user));
+
+// delete
+console.log("Delete", wm.delete(user));
+
+user = null;
+
+// Iterating
+
+const wm1 = new WeakMap();
+const keys = [];
+
+let user1 = { name: "Tom" };
+let user2 = { name: "Jerry" };
+
+wm1.set(user1, "data1");
+wm1.set(user2, "data2");
+
+keys.push(user1, user2);
+
+// iterate using array
+keys.forEach((key) => {
+  console.log(wm1.get(key));
+});
